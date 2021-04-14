2a4cb113ff7d7666dcba290676fbca53d722f309
gabrielmacaio <82482405+gabrielmacaio@users.noreply.github.com>
2021-04-13 22:10:04 -0300

    Apenas um teste

---
+++ script.js
@@ -0,0 +1,8 @@
+ function VerificarEntrada(){
+ 
+    let valor = document.getElementById('nome').value;
+    document.getElementById('PermissaoDeEntrada').innerText =  document.getElementById('nome').value;
+    document.getElementById('PermissaoDeEntrada2').innerText =  document.getElementById('nome').value;
+    document.getElementById('PermissaoDeEntrada3').innerText =  document.getElementById('nome').value;
+    document.getElementById('PermissaoDeEntrada4').innerText =  document.getElementById('nome').value;
+ }
